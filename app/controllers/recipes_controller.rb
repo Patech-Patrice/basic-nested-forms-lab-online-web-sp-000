450a94581740b4d827520cfccf6549e49a5f78f6
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-05-14 17:20:51 +0000

    Automatically backed up by Learn

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,28 @@
+class RecipesController < ApplicationController
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+    @recipe.ingredients.build(quantity: 0)
+    @recipe.ingredients.build(quantity: 0)
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,
+      ingredients_attributes: [:name, :quantity]
+    )
+  end
+end
